958d85a1e76006e12fc607a3e13eec0433a904e9
fathbsyr <fatihmubasyir0@gmail.com>
2023-06-28 22:28:46 +0700

    update navbar pateh pertama

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,45 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <div class="container">
 
-    {{-- @if (Auth::user()->role === "admin")
+        {{-- @if (Auth::user()->role === 'admin')
     <div class="card-body">
         Role: {{ Auth::user()->role }}
     </div>
     @endif --}}
-    <h1 class="mt-4">halaman User</h1>
-    <div class="row">
-        @foreach ($heroes as $hero)
-        <div class="col-sm-3">
-            <div class="card mb-4 shadow-sm">
-                <img class="card-img-top img-fluid product-image" src="/assets/img/{{ $hero->poster }}"
-                    alt="Card image cap">
-                <div class="card-block">
+        <h1 class="mt-4">halaman User</h1>
+        <div class="row">
+            @foreach ($heroes as $hero)
+                <div class="col-sm-3">
+                    <div class="card mb-4 shadow-sm">
+                        <img class="card-img-top img-fluid product-image" src="/assets/img/{{ $hero->poster }}"
+                            alt="Card image cap">
+                        <div class="card-block">
 
-                    <hr>
-                    <h4 class="card-title text-center mt-1 "><a href="#" class="text-decoration-none text-primary">
-                            {{ $hero->name }}
-                        </a>
-                    </h4>
-                    <div class="team-contact-info">
-                        <div class="text-center">
-                            <span class="text-decoration-line-through text-muted">Rp.
-                                {{ $hero->price*1.5 }}
-                            </span>
-                            Rp.
-                            {{ $hero->price }}
-                        </div>
-                        <div class="row m-4">
-                            <a href="#" class="btn btn-primary">Selengkapnya</a>
                             <hr>
+                            <h4 class="card-title text-center mt-1 "><a href="#"
+                                    class="text-decoration-none text-primary">
+                                    {{ $hero->name }}
+                                </a>
+                            </h4>
+                            <div class="team-contact-info">
+                                <div class="text-center">
+                                    <span class="text-decoration-line-through text-muted">Rp.
+                                        {{ $hero->price * 1.5 }}
+                                    </span>
+                                    Rp.
+                                    {{ $hero->price }}
+                                </div>
+                                <div class="row m-4">
+                                    <a href="#" class="btn btn-primary">Selengkapnya</a>
+                                    <hr>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-
-        @endforeach
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,56 +1,36 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-        <!-- Scripts -->
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-        <style>
-            #navbar {
-              background: rgb(184,0,0);
-              background: linear-gradient(90deg, rgba(184,0,0,1) 24%, rgba(2,0,158,1) 71%);
-            }
-    
-            .navbar-dark .nav-item .nav-link {
-                color: #fff;
-            }
-    
-            .navbar-dark .nav-item .nav-link:hover {
-                transition: all 0.3s ease;
-                border-radius: 0.25rem;
-                color: #fff;
-            }
-    
-            .fa-li {
-                position: relative;
-                left: 0;
-            }
-    
-        </style>
-    </head>
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
-    <body>
-        <div id="app">
-            <nav >
-                @include('partials.navfro')
-            </nav>
+    <link href="{{ asset('/assets/style_ui/style.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+</head>
 
-            <main class="py-4">
-                @yield('content')
-            </main>
-        </div>
-    </body>
+<body>
+    <div id="app">
+        <nav class="sb-nav-fixed">
+            @include('partials.navfro')
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
 
 </html>
